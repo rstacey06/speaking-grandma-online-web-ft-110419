cf3dcbf31629bc29152658a7dd7f35126ab30b1b
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 12:43:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase != phrase.upcase 
-    return "HUH?! SPEAK UP, SONNY!"
-    else "NO, NOT SINCE 1938!"
-  end
+ 
 end
 
   
